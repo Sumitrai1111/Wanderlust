a22e44f5374cbca4bb0743ea82c94fff410422fd
Sumitrai1111 <sumitrai81125@gmail.com>
2023-12-23 18:09:19 +0530

    Add Project Files

---
+++ routes/review.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");
+const {
+  validateReview,
+  isLoggedIn,
+  isreviewAuthor,
+} = require("../middleware.js");
+
+const reviewController = require("../controller/review.js");
+
+//Reviews Post Route
+router.post(
+  "/",
+  isLoggedIn,
+  validateReview,
+  wrapAsync(reviewController.createReview)
+);
+
+//Delete Reivew Route
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isreviewAuthor,
+  wrapAsync(reviewController.deleteReview)
+);
+
+module.exports = router;
